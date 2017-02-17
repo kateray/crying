ab0fa09b07ecd4a224f052955ed7d4eda11d0859
Kate Ray <email@kateray.net>
2017-02-17 14:46:56 +0200

    Style update

--- src/components/Magnify.jsx
+++ src/components/Magnify.jsx
@@ -11,7 +11,7 @@ class Magnify extends Component {
     const top = this.props.data.dragTop - height;
     const left = this.props.data.dragLeft;
     return (
-      <Map className="pink-map" style={{height: height, top: top, left: left}} zoomControl={false} attributionControl={false} opacity={0.5} zoom={15} center={position}>
+      <Map className="mini-map" style={{height: height, top: top, left: left}} zoomControl={false} attributionControl={false} opacity={0.5} zoom={15} center={position}>
         <TileLayer url='https://api.mapbox.com/styles/v1/kray/ciz6s7pe400162sqzis0pnxgz/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoia3JheSIsImEiOiJjaXoxZmdyZ3gwMDE1MnFvZG9oZmhrMTBsIn0.mvcEq1pLdeOv-xUSGn6sVw' />
         <Marker position={position} icon={icon} />
       </Map>
