6cdde7dbb54795486b9e465f6710bfa0b2311ae8
Kate Ray <email@kateray.net>
2017-02-18 12:30:36 +0200

    Submit pin form on enter

--- src/components/EmojiPin.jsx
+++ src/components/EmojiPin.jsx
@@ -7,6 +7,12 @@ class EmojiPin extends Component {
     this.leafletMap.leafletElement.openPopup();
   }
 
+  handleDescriptionKey(e) {
+    if (e.key === 'Enter') {
+      this.leafletMap.leafletElement.closePopup();
+    }
+  }
+
   render() {
     const icon = divIcon({className: 'emoji-marker', iconSize: 16, html: this.props.data.hex, popupAnchor: [90,0]});
     const position = [this.props.data.lat, this.props.data.lng];
@@ -18,7 +24,7 @@ class EmojiPin extends Component {
               <input type="text" value={this.props.data.title} onChange={this.props.handleTitleChange}/>
             </div>
             <div>
-              <textarea placeholder="What happened? (optional)" onChange={this.props.handleDescriptionChange} defaultValue={this.props.data.description}/>
+              <textarea placeholder="What happened? (optional)" onKeyPress={this.handleDescriptionKey.bind(this)} onChange={this.props.handleDescriptionChange} defaultValue={this.props.data.description}/>
             </div>
           </div>
         </Popup>
