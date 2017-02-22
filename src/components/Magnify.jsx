e613c51610a1c7c64b809aab453227b3859c7512
Kate Ray <email@kateray.net>
2017-02-22 14:38:56 +0200

    Switched magnify to satellite map

--- src/components/Magnify.jsx
+++ src/components/Magnify.jsx
@@ -11,8 +11,8 @@ class Magnify extends Component {
     const top = this.props.data.dragTop - height;
     const left = this.props.data.dragLeft;
     return (
-      <Map className="mini-map" style={{height: height, top: top, left: left}} zoomControl={false} attributionControl={false} opacity={0.5} zoom={15} center={position}>
-        <TileLayer url='https://api.mapbox.com/styles/v1/kray/cizcz53tq00bh2spmmkpwoqj6/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoia3JheSIsImEiOiJjaXoxZmdyZ3gwMDE1MnFvZG9oZmhrMTBsIn0.mvcEq1pLdeOv-xUSGn6sVw' />
+      <Map className="mini-map" style={{height: height, top: top, left: left}} zoomControl={false} attributionControl={false} opacity={0.5} zoom={16} center={position}>
+        <TileLayer url='https://api.mapbox.com/styles/v1/kray/cizgx9igt00cs2slm2etixmw4/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoia3JheSIsImEiOiJjaXoxZmdyZ3gwMDE1MnFvZG9oZmhrMTBsIn0.mvcEq1pLdeOv-xUSGn6sVw' />
         <Marker position={position} icon={emojiIcon} />
       </Map>
     )
