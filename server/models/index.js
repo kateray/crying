9f098c8b64c96890fe1a12dae9bc53087440736d
Kate Ray <email@kateray.net>
2017-04-27 15:02:41 -0400

    Tweak to sequelize config

--- server/models/index.js
+++ server/models/index.js
@@ -13,8 +13,6 @@ if (process.env.DATABASE_URL) {
   sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect:  'postgres',
     protocol: 'postgres',
-    port:     match[4],
-    host:     match[3],
     logging:  true //false
   })
 } else {
