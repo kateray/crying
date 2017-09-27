85ca8141262a8f494b00cad8ff83ca7263612691
Kate Ray <kateray@users.noreply.github.com>
2017-09-27 10:58:30 -0400

    Update instructions link to my pins

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -304,7 +304,7 @@ export class UserMap extends Component {
               Hello! This is a place for you to record stories about your personal moments in public spaces.
             </p>
             <p>
-              Your pins will be shown on the homepage, but not associated with you. You can share your map if you want though. <a target="_blank"  href="http://cryinginpublic.com/maps/qiwnw20tjo">Here's mine.</a>
+              Your pins will be shown on the homepage, but not associated with you. You can share your map if you want though. <a target="_blank"  href="http://cryinginpublic.com/maps/g1534fip1s">Here's mine.</a>
             </p>
             <p>
               {"<-- Grab and drag an emoji onto the map to get started."}
