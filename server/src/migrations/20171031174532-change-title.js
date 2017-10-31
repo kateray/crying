7568b571ca260da8da3f741b28e78c5d4ee8a9a8
Kate Ray <kateray@users.noreply.github.com>
2017-10-31 13:50:16 -0400

    Migration for making title datatype Text

---
+++ server/src/migrations/20171031174532-change-title.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.changeColumn(
+      'Pins',
+      'title',
+      {
+        type: Sequelize.TEXT
+      }
+    )
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.changeColumn(
+      'Pins',
+      'title',
+      {
+        type: Sequelize.STRING
+      }
+    )
+  }
+};
